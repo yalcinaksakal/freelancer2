1fe25e16d54bf821fd1350037e5c6871dd1a3a1e
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2022-09-07 15:18:08 +0200

    button

---
+++ src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import "./Button.styles.scss";
+
+const Button = ({ children, buttonType, ...otherProps }) => {
+	return (
+		<button className={`button-container ${buttonType}`} {...otherProps}>
+			{children}
+		</button>
+	);
+};
+
+export default Button;
--- src/components/PageContent/PageContent.js
+++ src/components/PageContent/PageContent.js
@@ -1,4 +1,20 @@
 const PageContent = ({ page }) => {
-	return <div>{page}</div>;
+	let content;
+	switch (page) {
+		case "logs":
+			content = <div>log</div>;
+			break;
+		case "add":
+			content = <div>add</div>;
+			break;
+		case "eval":
+			content = <div>eval</div>;
+			break;
+		default:
+			content = <div>Page not found!</div>;
+			break;
+	}
+
+	return content;
 };
 export default PageContent;
